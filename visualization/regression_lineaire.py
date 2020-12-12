8dfeec2b3d030f8752e8571cc615d25e2df4c088
Eric Vong <eric.vong@ensae.fr>
2020-12-12 21:56:40 +0100

    affichage régression linéaire

--- visualization/regression_lineaire.py
+++ visualization/regression_lineaire.py
@@ -30,7 +30,7 @@ reglinear = LinearRegression(fit_intercept=True,normalize=False)
 reglinear.fit(np.delete(donnees_meubles, yindex, axis = 1),donnees_meubles[:,yindex])
 reglinear.score(np.delete(donnees_meubles, yindex, axis = 1),donnees_meubles[:,yindex])
 
-pieces_meuble,distance_centre_meuble = reglinar.coef_
+pieces_meuble,distance_centre_meuble = reglinear.coef_
 ordonnee_meuble = reglinear.intercept_ #Le prix au m^2 descend bien avec le nombre de pièces, rendements marginaux décroissants
 
 reglinear.fit(np.delete(donnees_non_meubles, yindex, axis = 1),donnees_non_meubles[:,yindex])
@@ -40,17 +40,59 @@ pieces_non_meuble,distance_centre_non_meuble =reglinear.coef_
 ordonnee_non_meuble = reglinear.intercept_ #Même commentaire
 #Ordonnée à l'origine plus haute pour meublé, c'est ce à quoi on s'attend
 
-###Visualisation de la régression
-ref_meuble,piec_meuble,dist_meuble = donnees_meubles[:,0],donnees_meubles[:,1],donnees_meubles[:,2]
-ref_non_meuble,piec_non_meuble,dist_non_meuble = donnees_non_meubles[:,0],donnees_non_meubles[:,1],donnees_non_meubles[:,2]
+#Visualisation de la régression
+
+xx,yy=np.meshgrid(range(8),range(8))
+z_meuble = xx*pieces_meuble+yy*distance_centre_meuble+ordonnee_meuble
+z_non_meuble = xx*pieces_non_meuble+yy*distance_centre_non_meuble+ordonnee_non_meuble
+
+plt3d = plt.figure().gca(projection='3d')
+plt3d.plot_surface(xx, yy, z_meuble)
+plt3d.plot_surface(xx, yy, z_non_meuble)
+plt.show()
 
 #Visualisation des points 
 
+ref_meuble,piec_meuble,dist_meuble = donnees_meubles[:,0],donnees_meubles[:,1],donnees_meubles[:,2]
+ref_non_meuble,piec_non_meuble,dist_non_meuble = donnees_non_meubles[:,0],donnees_non_meubles[:,1],donnees_non_meubles[:,2]
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(piec_meuble,dist_meuble,ref_meuble,c='red')
-ax.scatter(piec_non_meuble,dist_non_meuble,ref_non_meuble,c='blue')
+ax.scatter(piec_meuble,dist_meuble,ref_meuble,c='red',label='Meublé')
+ax.scatter(piec_non_meuble,dist_non_meuble,ref_non_meuble,c='blue',label='Non meublé')
 ax.set_xlabel('Nombre de pièce')
 ax.set_ylabel('Distance du centre de Paris')
 ax.set_zlabel('Prix du m^2')
+plt.show()
+
+#Régression linéaire meublé
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+plt3d = plt.figure().gca(projection='3d')
+plt3d.plot_surface(xx, yy, z_meuble)
+ax = plt.gca()
+ax.hold(True)
+ax.scatter(piec_meuble,dist_meuble,ref_meuble,c='red')
+ax.set_xlabel('Nombre de pièce')
+ax.set_ylabel('Distance du centre de Paris')
+ax.set_zlabel('Prix du m^2')
+plt.title('Régression linéaire pour les appartements meublés')
+plt.legend()
+plt.show()
+
+#Régression linéaire non meublé
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+plt3d = plt.figure().gca(projection='3d')
+plt3d.plot_surface(xx, yy, z_non_meuble)
+ax = plt.gca()
+ax.hold(True)
+ax.scatter(piec_non_meuble,dist_non_meuble,ref_non_meuble,c='red')
+ax.set_xlabel('Nombre de pièce')
+ax.set_ylabel('Distance du centre de Paris')
+ax.set_zlabel('Prix du m^2')
+plt.title('Régression linéaire pour les appartements non meublés')
+plt.legend()
 plt.show()
