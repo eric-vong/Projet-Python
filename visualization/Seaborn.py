6a455b9bbcae340da088f2416ae5eefcea8ee365
thibautvalour <74783392+thibautvalour@users.noreply.github.com>
2020-12-13 23:01:40 +0100

    Update Seaborn.py

--- visualization/Seaborn.py
+++ visualization/Seaborn.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import geopandas as gpd
 import contextily as ctx
@@ -14,10 +8,6 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import LabelBinarizer
 import descartes
 
-
-# In[2]:
-
-
 #On importe les données, puis on transforme la variable textuelle "meuble_text" en variable binaire "meuble_bin"
 #On choisit également de ne garder que la variable ref comme indicateur de prix car max et min sont une transformation affine de celle-ci.
 donnees = gpd.read_file('donnees_augmentees.geojson')
@@ -26,15 +16,8 @@ donnees=donnees.drop(['meuble_txt','max','min','id_quartier'],axis=1)
 donnees.rename(columns={'ref': 'Prix_m2'}, inplace=True)
 donnees.sample(5)
 
-
-# In[3]:
-
-
 #Matrice de correlation
 sns.heatmap(donnees.corr(),annot = True,cmap='coolwarm',fmt='.1g')
-
-
-# In[4]:
 
 
 sns.boxplot(x="piece", y="Prix_m2", data=donnees,
@@ -44,22 +27,13 @@ sns.boxplot(x="piece", y="Prix_m2", data=donnees,
 #Comme on pouvait s'y attendre, le prix baisse, mais de moins en moins.
 
 
-# In[5]:
-
-
 sns.boxplot(x="meuble_bin", y="Prix_m2", data=donnees,
             whis=[0, 100], width=.6)
 #Le prix du mètre carré d'un appartement meublé est plus cher que celui d'un non meublé.
 
 
-# In[6]:
-
-
 donnees=donnees.drop(['meuble_bin'],axis=1)
 sns.pairplot(donnees,hue='piece',palette='bright',height=1.9)
-
-
-# In[7]:
 
 
 donnees = gpd.read_file('donnees_augmentees.geojson')
@@ -69,19 +43,12 @@ donnees.rename(columns={'ref': 'Prix_m2'}, inplace=True)
 sns.pairplot(donnees,hue='meuble_bin',palette='bright',height=1.9)
 
 
-# In[8]:
-
-
 #On réimporte les données pour avoir la base initiales 
 donnees = gpd.read_file('donnees_augmentees.geojson')
 donnees['meuble_bin'] = 1 - LabelEncoder().fit_transform(donnees['meuble_txt'])
 donnees=donnees.drop(['meuble_txt','max','min','id_quartier'],axis=1)
 donnees.rename(columns={'ref': 'Prix_m2'}, inplace=True)
 donnees.sample(5)
-
-
-# In[26]:
-
 
 donnees.plot(column = "Prix_m2",cmap = 'coolwarm',legend = True,
              legend_kwds = {'label':'Prix moyen au mètre carré (€)'},
@@ -98,16 +65,10 @@ donnees[donnees['meuble_bin'] == 0].plot(column = "Prix_m2",cmap = 'coolwarm',le
 #Carte des prix pour les appartements non meublés
 
 
-# In[11]:
-
-
 donnees.plot(column = "distance_centre_paris",legend = True,
              legend_kwds = {'label':'Distance au centre de Paris (km)'},
             figsize = (10,6))
 #Vérification graphique de la distance au centre de Paris
-
-
-# In[25]:
 
 
 donnees.plot(column = "piece",cmap = 'coolwarm',legend = True,
