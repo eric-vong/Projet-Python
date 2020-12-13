4979ef16848ff61814547791eb26c11861cb5bfd
Eric Vong <eric.vong@ensae.fr>
2020-12-13 16:04:25 +0100

    ajout régression linéaire + commentaire

--- visualization/regression_lineaire.py
+++ visualization/regression_lineaire.py
@@ -12,6 +12,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 import sys
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 todrop = ['q_'+str(i+1) for i in range(80)]+['geometry','min','max','superficie_quartier','score_metro','epoque_1','epoque_2','epoque_3','epoque_0']
 donnees.drop(todrop,axis=1,inplace=True)
 
@@ -40,9 +41,19 @@ pieces_non_meuble,distance_centre_non_meuble =reglinear.coef_
 ordonnee_non_meuble = reglinear.intercept_ #Même commentaire
 #Ordonnée à l'origine plus haute pour meublé, c'est ce à quoi on s'attend
 
+ref_meuble,piec_meuble,dist_meuble = donnees_meubles[:,0],donnees_meubles[:,1],donnees_meubles[:,2]
+ref_non_meuble,piec_non_meuble,dist_non_meuble = donnees_non_meubles[:,0],donnees_non_meubles[:,1],donnees_non_meubles[:,2]
+
+ref_predict_meuble = piec_meuble*pieces_meuble+dist_meuble*distance_centre_meuble+ordonnee_meuble
+ref_predict_non_meuble = piec_meuble*pieces_non_meuble+dist_meuble*distance_centre_non_meuble+ordonnee_non_meuble
+
+mean_squared_error(ref_meuble,ref_predict_meuble)
+mean_squared_error(ref_non_meuble,ref_predict_non_meuble)
+
+
 #Visualisation de la régression
 
-xx,yy=np.meshgrid(range(8),range(8))
+xx,yy=np.meshgrid(range(5),range(8))
 z_meuble = xx*pieces_meuble+yy*distance_centre_meuble+ordonnee_meuble
 z_non_meuble = xx*pieces_non_meuble+yy*distance_centre_non_meuble+ordonnee_non_meuble
 
@@ -53,9 +64,6 @@ plt.show()
 
 #Visualisation des points 
 
-ref_meuble,piec_meuble,dist_meuble = donnees_meubles[:,0],donnees_meubles[:,1],donnees_meubles[:,2]
-ref_non_meuble,piec_non_meuble,dist_non_meuble = donnees_non_meubles[:,0],donnees_non_meubles[:,1],donnees_non_meubles[:,2]
-
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(piec_meuble,dist_meuble,ref_meuble,c='red',label='Meublé')
@@ -64,6 +72,8 @@ ax.set_xlabel('Nombre de pièce')
 ax.set_ylabel('Distance du centre de Paris')
 ax.set_zlabel('Prix du m^2')
 plt.show()
+
+#On observe que les plans sont à peu près parallèles, ce qui est conforme à notre intuition et que le plan qui comporte les données meublées est plus haut, ce qui est aussi conforme à notre intuition.
 
 #Régression linéaire meublé
 
